194a6d8b4cda886e3552676ab09d2cfc63350797
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-05 15:52:51 +0530

    fff

--- pages/addProfile/[slug].js
+++ pages/addProfile/[slug].js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { toast } from "react-toastify";
 
 function FormPage() {
--- pages/signUp.js
+++ pages/signUp.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { toast } from "react-toastify";
 
 function SignUp() {
