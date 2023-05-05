5c2eb357f529662cddb2778c07b37ba4d1425160
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-05 16:35:04 +0530

    toastifyfixno

--- pages/_app.js
+++ pages/_app.js
@@ -4,20 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      >
-        <Component {...pageProps} />
-      </ToastContainer>
+      <Component {...pageProps} />
     </>
   );
 }
