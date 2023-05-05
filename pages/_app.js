2d22c59f363ad10660f2bea66d1b8a36fadfec6a
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-05 16:25:35 +0530

    fffdswjj2

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
 import "@/styles/globals.css";
-import { ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <ToastContainer />
       <Component {...pageProps} />
     </>
   );
